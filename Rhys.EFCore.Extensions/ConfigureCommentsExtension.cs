c30c66aa02851a0ccc75dd9b856fc350346cb1f4
wangrusheng <wangrusheng@wy5u.com>
2023-06-02 17:36:31 +0800

    添加项目文件。

---
+++ Rhys.EFCore.Extensions/ConfigureCommentsExtension.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rhys.EFCore.Extensions.Core;
+
+namespace Rhys.EFCore.Extensions
+{
+    public static class ConfigureCommentsExtension
+    {
+
+        /// <summary>
+        /// 配置实体类对应数据表的注释
+        /// </summary>
+        /// <param name="entityTypeBuilder"></param>
+        public static void TryConfigureEntityComment<T>(this EntityTypeBuilder<T> entityTypeBuilder) where T : class
+        {
+            try
+            {
+                var dic = XmlDocumentHelper.GetPropCommentByClassType<T>();
+
+                //Config Class Name Comment to Database Table Name Comment
+                entityTypeBuilder.ToTable(c => c.HasComment(dic[typeof(T).FullName ?? "className"]));
+
+                //Config Property Comment to Database Column Comment
+                foreach (var property in typeof(T).GetProperties())
+                {
+                    var propName = property.Name;
+                    var comment = dic[propName];
+                    var lambda = LambdaExpressionGenerator.CreateLambdaExpression<T>(property);
+                    EntityTypeBuilderConfig(property, lambda, entityTypeBuilder, comment);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void EntityTypeBuilderConfig<T>(PropertyInfo property, LambdaExpression lambda,
+            EntityTypeBuilder<T> b, string? comment) where T : class
+        {
+
+            try
+            {
+                if (typeof(Guid) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, Guid>>)lambda).HasComment(comment);
+                }
+                else if (typeof(Guid?) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, Guid?>>)lambda).HasComment(comment);
+                }
+                else if (typeof(string) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, string>>)lambda).HasComment(comment);
+                }
+                else if (typeof(int) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, int>>)lambda).HasComment(comment);
+                }
+                else if (typeof(bool) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, bool>>)lambda).HasComment(comment);
+                }
+                else if (typeof(DateTime?) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, DateTime?>>)lambda).HasComment(comment);
+                }
+                else if (typeof(DateTime) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, DateTime>>)lambda).HasComment(comment);
+                }
+                else if (typeof(Enum) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, Enum>>)lambda).HasComment(comment);
+                }
+                else if (typeof(long) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, long>>)lambda).HasComment(comment);
+                }
+                else if (typeof(long?) == property.PropertyType)
+                {
+                    b.Property((Expression<Func<T, long?>>)lambda).HasComment(comment);
+                }
+                // config  your customer type here
+                else
+                {
+                    var currentBackgroundColor = Console.BackgroundColor;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(@$"{property.PropertyType}  the property data type is not matched, you need to add a type configuration  ");
+                    Console.BackgroundColor = currentBackgroundColor;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Failed to set comment field:{e.Message}");
+            }
+        }
+
+    }
+}
---
+++ Rhys.EFCore.Extensions/Core/LambdaExpressionGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rhys.EFCore.Extensions.Core
+{
+    internal class LambdaExpressionGenerator
+    {
+        public static LambdaExpression CreateLambdaExpression<T>(PropertyInfo property)
+        {
+            // 创建实体类的参数表达式
+            ParameterExpression param = Expression.Parameter(typeof(T), "e");
+            // 创建属性访问的成员表达式
+            MemberExpression propertyAccess = Expression.Property(param, property);
+            // 创建 Lambda 表达式
+            LambdaExpression lambda = Expression.Lambda(propertyAccess, param);
+            // 转换 Lambda 表达式为具体的泛型类型
+            return lambda;
+        }
+    }
+}
